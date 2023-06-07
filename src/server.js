b6f00968d173f9ad660609fa59959ee803ca1b21
JamshidBek <94694648+husanovjamshid@users.noreply.github.com>
2023-06-07 17:14:03 +0500

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log('server is running', PORT));
